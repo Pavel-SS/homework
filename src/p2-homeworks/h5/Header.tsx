3f0dd0392a30ca22716bf9aae4945cc390944081
Pavel <sosnovskiy.pavel@inbox.ru>
2021-12-19 17:44:33 +0300

    Update React-Router

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -3,8 +3,7 @@ import React from 'react'
 function Header() {
     return (
         <div>
-            // add NavLinks
-
+             add NavLinks
         </div>
     )
 }
--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -1,29 +1,30 @@
 import React from 'react'
+import Error404 from './pages/Error404'
+import PreJunior from './pages/PreJunior'
+import {NavLink, Routes, Route } from 'react-router-dom'
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
     // add paths
 }
 
-function Routes() {
+function RoutesFunc() {
     return (
         <div>
             {/*Switch выбирает первый подходящий роут*/}
-            {/*<Switch>*/}
-
+            <NavLink to={'/pre-junior'}>pre-junior</NavLink>
+            <Routes>
             {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
             {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
             {/*<Route path={'/'} exact render={() => <Redirect to={PATH.PRE_JUNIOR}/>}/>*/}
 
-            {/*<Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>*/}
-                // add routes
-
+            <Route path={'/pre-junior'} element={<PreJunior/>}/>
             {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-            {/*<Route render={() => <Error404/>}/>*/}
+            <Route element={() => <Error404/>}/>
 
-            {/*</Switch>*/}
+            </Routes>
         </div>
     )
 }
 
-export default Routes
+export default RoutesFunc
