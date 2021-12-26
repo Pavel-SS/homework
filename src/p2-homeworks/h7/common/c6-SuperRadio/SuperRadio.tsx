d956aad61111b2c6ac1cc4596edda95ca90a1220
Pavel <sosnovskiy.pavel@inbox.ru>
2021-12-26 19:43:39 +0300

    Update SuperRadio.tsx

--- src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
+++ src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
@@ -9,24 +9,34 @@ type SuperRadioPropsType = DefaultRadioPropsType & {
 
 const SuperRadio: React.FC<SuperRadioPropsType> = (
     {
-        type, name,
-        options, value,
-        onChange, onChangeOption,
+        type, 
+        name,
+        options, 
+        value,
+        onChange, 
+        onChangeOption,
         ...restProps
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         // onChange, onChangeOption
+        onChange && onChange(e)
+        onChangeOption && onChangeOption(e.currentTarget.value)
     }
 
 
-    const mappedOptions: any[] = options ? options.map((o, i) => ( // map options with key
+    const mappedOptions: any[] = options ? options.map((item, i) => ( // map options with key
         <label key={name + '-' + i}>
             <input
                 type={'radio'}
-                // name, checked, value, onChange
+                // name, checked, value,
+                name = {name}
+                value = {item}
+                checked = {item === value}
+                onChange = {onChangeCallback}
+                {...restProps}
             />
-            {o}
+            {item}
         </label>
     )) : []
 
