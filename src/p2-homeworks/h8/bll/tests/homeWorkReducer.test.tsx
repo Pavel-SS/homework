f077cf1232d6103ec9676e6f1f07d524f396fdf4
Pavel <sosnovskiy.pavel@inbox.ru>
2022-01-20 21:09:03 +0300

    Update homeWorkReducer.test.tsx

--- src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
+++ src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import {homeWorkReducer} from '../homeWorkReducer'
+import { UserType } from "../../HW8"
 
-let initialState: any[] // need to fix any
+
+let initialState: UserType[]
 
 beforeEach(() => {
     initialState = [
@@ -17,16 +19,15 @@ beforeEach(() => {
 test('sort name up', () => {
     const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'up'})
 
-    console.log(newState)
-    // expect(...).toBe(...)
+    expect(newState[0].name).toBe("Александр")
 })
 test('sort name down', () => {
     const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'down'})
 
-
+    expect(newState[0].name).toBe("Кот")
 })
 test('check age 18', () => {
     const newState = homeWorkReducer(initialState, {type: 'check', payload: 18})
 
-
+    expect(newState.length).toBe(4)
 })
