e49b440fd0281b9b89d667a3cfd1853d1a9e7544
Pavel <sosnovskiy.pavel@inbox.ru>
2022-01-20 21:08:13 +0300

    Update SuperEditableSpan.tsx

--- src/p2-homeworks/h6/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ src/p2-homeworks/h6/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -1,5 +1,6 @@
 import React, {DetailedHTMLProps, InputHTMLAttributes, HTMLAttributes, useState} from 'react'
 import SuperInputText from '../../../h4/common/c1-SuperInputText/SuperInputText'
+import s from './SuperEditableSpan.module.css'
 
 // тип пропсов обычного инпута
 type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
@@ -44,13 +45,14 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
         onDoubleClick && onDoubleClick(e)
     }
 
-    const spanClassName = `${'сделать красивый стиль для спана'} ${className}`
+     const spanClassName = `${s.span}  ${className}`
+
 
     return (
         <>
             {editMode
                 ? (
-                    <SuperInputText
+                    <SuperInputText 
                         autoFocus // пропсу с булевым значением не обязательно указывать true
                         onBlur={onBlurCallback}
                         onEnter={onEnterCallback}
