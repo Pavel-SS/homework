0c2581f92361565b75e9a93291d866f15ec11bc2
Pavel <sosnovskiy.pavel@inbox.ru>
2022-01-23 23:22:09 +0300

    Update AlternativeClock.tsx

--- src/p2-homeworks/h9/AlternativeClock.tsx
+++ src/p2-homeworks/h9/AlternativeClock.tsx
@@ -2,33 +2,61 @@
 import moment, { Moment } from 'moment'
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import {v1} from 'uuid';
 import './Style.scss'
-    
-function AlternativeClock() {
-    const [date, setDate] = useState <Moment> (moment)
-    const [id, setId] = useState <number> (0)
+import s from './Style.module.scss'
+// function AlternativeClock() {
+//     const [date, setDate] = useState <Moment> (moment)
+//     const [id, setId] = useState <number> (0)
 
-    const sH = () => {console.log(id)
-        if ( id === 0){ 
-            clearInterval(id)
-            setId(window.setInterval(() => {
-                setDate(moment())
-            }, 1000))
-        }else{
-            setId(0)
-            clearInterval(id)
+//     const sH = () => {console.log(id)
+//         if ( id === 0){ 
+//             clearInterval(id)
+//             setId(window.setInterval(() => {
+//                 setDate(moment())
+//             }, 1000))
+//         }else{
+//             setId(0)
+//             clearInterval(id)
+//         }
+//     }
+
+//     const d = date?.format('LTS') || <span>00:00:00</span>;
+    
+//     return ( 
+//         <div>
+//             <SuperButton onClick={sH}>Show Clock</SuperButton>
+//             <span className={id === 0 ? 'show': ''}>{ d }</span>
+//         </div>
+//     )
+  
+// }
+
+
+function AlternativeClock(){
+    const [x, setX] = useState<Array<number>>([]);
+        const xor = [];
+        for(let i = 1; i <= 12; i++){
+            xor.push(i);  
         }
-    }
+        if (x.length === 12){
+            setX(xor)
+        }
+       const num = xor.map(item => {
+           let num = +item
+         return (<div className={'number'+' '+'number'+String(num)}>{item}</div>)
+        })
 
-    const d = date?.format('LTS') || <span>00:00:00</span>;
-    
-    return ( 
-        <div>
-            <SuperButton onClick={sH}>Show Clock</SuperButton>
-            <span className={id === 0 ? 'show': ''}>{ d }</span>
+   console.log(x);
+
+   
+    return (
+        <div className='alternativeClock'>
+            <div className={s.hour}></div>
+            <div className={s.minut}></div>
+            <div className={s.second}></div>
+            {num}
         </div>
     )
-  
 }
-
 export default AlternativeClock
