29895c652839bfa973b9a424cd5a0208bffdc186
Pavel <sosnovskiy.pavel@inbox.ru>
2022-01-23 17:05:33 +0300

    Update AlternativeClock.tsx

--- src/p2-homeworks/h9/AlternativeClock.tsx
+++ src/p2-homeworks/h9/AlternativeClock.tsx
@@ -1,13 +1,44 @@
 import React from 'react'
 
+<<<<<<< Updated upstream
 function AlternativeClock() {
 
 
     return (
         <div>
 
+=======
+import moment, { Moment } from 'moment'
+import React, {useState} from 'react'
+import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import './AlternativeClock.scss'
+    
+function AlternativeClock() {
+    const [date, setDate] = useState <Moment> (moment)
+    const [id, setId] = useState <number> (0)
+
+    const sH = () => {console.log(id)
+        if ( id === 0){ 
+            clearInterval(id)
+            setId(window.setInterval(() => {
+                setDate(moment())
+            }, 1000))
+        }else{
+            setId(0)
+            clearInterval(id)
+        }
+    }
+
+    const d = date?.format('LTS') || <span>00:00:00</span>;
+    
+    return ( 
+        <div>
+            <SuperButton onClick={sH}>Show Clock</SuperButton>
+            <span className={id === 0 ? 'show': ''}>{ d }</span>
+>>>>>>> Stashed changes
         </div>
     )
+  
 }
 
 export default AlternativeClock
