da758f2a8e5222b8ab445b921497364283582ca3
Pavel <sosnovskiy.pavel@inbox.ru>
2022-03-22 23:05:40 +0300

    Update HW12.tsx

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import SuperRadio from "../h7/common/c6-SuperRadio/SuperRadio";
+import { ThemeReducerStateType,  ThemeStyleType } from "./bll/themeReducer";
 import s from "./HW12.module.css";
 
 const themes = ['dark', 'red', 'some'];
 
 function HW12() {
     const theme = 'some'; // useSelector
-
+    // const theme = useSelector<ThemeReducerStateType,ThemeStyleType>(state=>state.themeBlock)
     // useDispatch, onChangeCallback
 
     return (
@@ -14,7 +17,9 @@ function HW12() {
             <span className={s[theme + '-text']}>
                 homeworks 12
             </span>
-
+            <div>
+                <SuperRadio name={'radio'}/>
+            </div>
             {/*should work (должно работать)*/}
             {/*SuperSelect or SuperRadio*/}
 
