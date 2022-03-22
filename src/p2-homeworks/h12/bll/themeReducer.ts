98f6be2a03df994b181620f265bc28756612a4e6
Pavel <sosnovskiy.pavel@inbox.ru>
2022-03-22 23:05:36 +0300

    Update themeReducer.ts

--- src/p2-homeworks/h12/bll/themeReducer.ts
+++ src/p2-homeworks/h12/bll/themeReducer.ts
@@ -1,14 +1,23 @@
+export type ThemeStyleType = 'dark' | 'red' | 'some'
+
 const initState = {
-
+     themeBlock: 'some' as ThemeStyleType
 };
-
-export const themeReducer = (state = initState, action: any): any => { // fix any
+ 
+type ActionType = {
+    type: 'CHANGE-THEME'
+    theme: ThemeStyleType
+}
+export const themeReducer = (state = initState, action: ActionType): typeof initState => { // fix any
     switch (action.type) {
-        case "": {
+        case 'CHANGE-THEME': {
             return state;
         }
         default: return state;
     }
 };
 
-export const changeThemeC = (): any => {}; // fix any
+export const changeThemeAC = (theme: ThemeStyleType): ActionType => ({type:'CHANGE-THEME', theme}); // fix any
+
+
+export type ThemeReducerStateType = ReturnType<typeof themeReducer>
