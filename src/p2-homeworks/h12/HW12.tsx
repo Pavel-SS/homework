7f217c19351c784653d312025a03d579d85f8960
Pavel <sosnovskiy.pavel@inbox.ru>
2022-03-23 23:39:01 +0300

    Update HW12.tsx

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -1,30 +1,32 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState } from "react";
+import { Provider, useDispatch, useSelector } from "react-redux";
 import SuperRadio from "../h7/common/c6-SuperRadio/SuperRadio";
-import { ThemeReducerStateType,  ThemeStyleType } from "./bll/themeReducer";
+import { changeThemeAC, initState, ThemeReducerStateType,  ThemeStyleType } from "./bll/themeReducer";
 import s from "./HW12.module.css";
-
+import {store} from "./store"
 const themes = ['dark', 'red', 'some'];
 
 function HW12() {
+    const [x, setX] = useState(initState.themeBlock)
     const theme = 'some'; // useSelector
     // const theme = useSelector<ThemeReducerStateType,ThemeStyleType>(state=>state.themeBlock)
     // useDispatch, onChangeCallback
-
+    const dispatch = useDispatch()
     return (
-        <div className={s[theme]}>
-            <hr/>
-            <span className={s[theme + '-text']}>
-                homeworks 12
-            </span>
-            <div>
-                <SuperRadio name={'radio'}/>
+            <div className={s[x]}>
+                <hr />
+                <span className={s[x + '-text' ]}>
+                    homeworks 12
+                </span>
+                {/*should work (должно работать)*/}
+                {/*SuperSelect or SuperRadio*/}
+                <br/>
+                <Provider store={store} >
+                     <SuperRadio name={'radio'} value={x} options={themes} onChangeOption={()=>dispatch(changeThemeAC(theme))} />
+                </Provider>
+                <hr />
             </div>
-            {/*should work (должно работать)*/}
-            {/*SuperSelect or SuperRadio*/}
 
-            <hr/>
-        </div>
     );
 }
 
