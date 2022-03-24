74f08faa3c20177e01a9e0090834656d23f71eb9
Pavel <sosnovskiy.pavel@inbox.ru>
2022-03-24 23:04:13 +0300

    fix: bag

--- src/p2-homeworks/h12/store.ts
+++ src/p2-homeworks/h12/store.ts
@@ -1,8 +1,11 @@
+import { TypedUseSelectorHook, useSelector } from "react-redux";
 import { applyMiddleware, combineReducers, createStore } from "redux";
-import { themeReducer } from "./bll/themeReducer";
+import {  themeReducer } from "./bll/themeReducer";
+
 
 const rootReducer = combineReducers({
     themes: themeReducer
 })
-
-export const store = createStore(rootReducer)
+export type ThemeStateType = ReturnType<typeof rootReducer>
+export const useThemeSelector: TypedUseSelectorHook<ThemeStateType> = useSelector
+export const store = createStore(rootReducer, applyMiddleware())
