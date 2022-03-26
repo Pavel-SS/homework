f66d0e80deb9c1cc27ba3f3864cf402dca6eb8e0
Pavel <sosnovskiy.pavel@inbox.ru>
2022-03-26 20:37:41 +0300

    Update HW12.tsx

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -1,23 +1,31 @@
 import React from "react";
-import { Provider, useDispatch } from "react-redux";
+import {  useDispatch } from "react-redux";
 import SuperRadio from "../h7/common/c6-SuperRadio/SuperRadio";
 import { changeThemeAC, ThemeStyleType } from "./bll/themeReducer";
 import s from "./HW12.module.css";
-import {store, useThemeSelector} from "./store"
+import {useThemeSelector} from "../h10/bll/store"
 
 
 const themes = ['dark', 'red', 'some'];
 
 function HW12() {
     // const theme = 'some'; // useSelector
-    const theme = useThemeSelector<ThemeStyleType>(state=>state.themes.themeBlock)
+    const theme = useThemeSelector<ThemeStyleType>(state=>state.theme.themeBlock)
     // useDispatch, onChangeCallback
     const dispatch = useDispatch()
-    const onChangeCallback = () => {
-        dispatch(changeThemeAC(theme))
+    const onChangeCallback = (theme: ThemeStyleType) => {
+            if(theme === 'dark') 
+            {
+                dispatch(changeThemeAC(theme))
+            }
+            if (theme === 'red') {
+                dispatch(changeThemeAC(theme))
+            }
+            if (theme === 'some'){
+                dispatch(changeThemeAC(theme))
+            }
     }
     return ( 
-    <Provider store={store}>
         <div className={s[theme]}>
             <hr />
             <span className={s[theme + '-text' ]}>
@@ -26,10 +34,9 @@ function HW12() {
             {/*should work (должно работать)*/}
             {/*SuperSelect or SuperRadio*/}
             <br />
-            <SuperRadio name={'radio'} value={theme} options={themes} onChangeOption={()=>onChangeCallback()} />
+            <SuperRadio  value={theme} options={themes} onChangeOption={onChangeCallback} />
             <hr />
         </div>
-    </Provider>
     );
 }
 
