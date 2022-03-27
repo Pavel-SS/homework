9d91431ce68cd549a02a8114a03ef9da1c40b242
Pavel <sosnovskiy.pavel@inbox.ru>
2022-03-27 18:22:59 +0300

    Create HW13.tsx

---
+++ src/p2-homeworks/h13/HW13.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import  Request  from "./request/Request";
+import s from './HW13.module.scss'
+
+
+export const HW13 = () => {
+    return (
+    <div className={s.container}>
+        <Request/>
+    </div>
+    )
+}
